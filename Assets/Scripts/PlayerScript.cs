81e004cc230b8dbf10485d5625a6c06482f778ee
Poornajith <chiranthapoornajith@gmail.com>
2022-11-06 13:16:06 +0530

    Walking direction bug fix

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Timeline.AnimationPlayableAsset;
 
 public class PlayerScript : MonoBehaviour
 {
     [Header("Player Movements")]
     public float playerSpeed = 1.9f;
+    public float playerSprint = 3f;
 
     [Header("Player Script Camera")]
     public Transform playerCamera;
@@ -13,6 +15,7 @@ public class PlayerScript : MonoBehaviour
     [Header("Player Animator and Gravity")]
     public CharacterController cC;
     public float gravity = -9.81f;
+    public Animator animator;
 
     [Header("Player Jumping and Velocity")]
     public float turnCalmTime = 0.1f;
@@ -28,7 +31,9 @@ public class PlayerScript : MonoBehaviour
     {
         onSurface = Physics.CheckSphere(surfaceCheck.position, surfaceDistance, surfaceMask);
 
-        if(onSurface && velocity.y < 0)
+        Cursor.lockState = CursorLockMode.Locked;
+
+        if (onSurface && velocity.y < 0)
         {
             velocity.y = -2f;
         }
@@ -40,6 +45,8 @@ public class PlayerScript : MonoBehaviour
         playerMove();
 
         jump();
+
+        sprint();
     }
 
     void playerMove()
@@ -51,12 +58,24 @@ public class PlayerScript : MonoBehaviour
 
         if(direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
+            animator.SetBool("Idle", false);
+            animator.SetBool("Walk", true);
+            animator.SetBool("RifleWalk", false);
+            animator.SetBool("IdleAim", false);
+            animator.SetBool("Running", false);
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnCalmVelocity, turnCalmTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            cC.Move(direction.normalized * playerSpeed * Time.deltaTime);
+            cC.Move(moveDirection.normalized * playerSpeed * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool("Idle", true);
+            animator.SetBool("Running", false);
+            animator.SetBool("Walk", false);
         }
     }
 
@@ -64,7 +83,44 @@ public class PlayerScript : MonoBehaviour
     {
         if(Input.GetButtonDown("Jump") && onSurface)
         {
+            animator.SetBool("Idle", false);
+            animator.SetTrigger("Jump");
             velocity.y = Mathf.Sqrt(jumpRange * -2 * gravity);
+        }
+        else
+        {
+            animator.SetBool("Idle", true);
+            animator.ResetTrigger("Jump");
+        }
+    }
+
+    void sprint()
+    {
+        if(Input.GetButton("Sprint") && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) && onSurface)
+        {
+         
+            animator.SetBool("Walk", false);        
+            animator.SetBool("Running", true);
+
+            float horizontal_axis = Input.GetAxisRaw("Horizontal");
+            float vertical_axis = Input.GetAxisRaw("Vertical");
+
+            Vector3 direction = new Vector3(horizontal_axis, 0f, vertical_axis).normalized;
+
+            if (direction.magnitude >= 0.1f)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnCalmVelocity, turnCalmTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                cC.Move(moveDirection.normalized * playerSprint * Time.deltaTime);
+            }
+            else
+            {
+                animator.SetBool("Walk", true);
+                animator.SetBool("Running", false);
+            }
         }
     }
 }
