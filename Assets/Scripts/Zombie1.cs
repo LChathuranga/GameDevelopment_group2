80d29429fd5a2946ea5591d23c1937613b6329c2
LChathuranga <65799662+LChathuranga@users.noreply.github.com>
2022-11-09 17:25:52 +0530

    zombie

---
+++ Assets/Scripts/Zombie1.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;   
+
+public class Zombie1 : MonoBehaviour
+{
+    [Header("Zombie Things")]
+    public NavMeshAgent zombieAgent;
+    public Transform playerBody;
+    public Transform LookPoint;
+    public LayerMask PlayerLayer;
+
+    [Header("Zombie guarding Var")]
+    public GameObject[] walkPoints;
+    int currentZombiePosition = 0;
+    public float zombieSpeed;
+    float walkingPointRadius = 2;
+
+    [Header("Zombie mood/state")]
+    public float visionRadius;
+    public float attackingRadius;
+    public bool playerInVisionRadius;
+    public bool playerInAttackingRadius;
+
+    private void Awake()
+    {
+        zombieAgent = GetComponent<NavMeshAgent>();
+    }
+    private void Update()
+    {
+        playerInVisionRadius = Physics.CheckSphere(transform.position, visionRadius, PlayerLayer);
+        playerInAttackingRadius = Physics.CheckSphere(transform.position, attackingRadius, PlayerLayer);
+
+        if(!playerInVisionRadius && !playerInAttackingRadius) Guard();
+        if(playerInVisionRadius && !playerInAttackingRadius) Pursueplayer();
+    }
+    private void Guard()
+    {
+        if(Vector3.Distance(walkPoints[currentZombiePosition].transform.position, transform.position) < walkingPointRadius)
+        {
+            currentZombiePosition = Random.Range(0, walkPoints.Length);
+            if(currentZombiePosition >= walkPoints.Length)
+            {
+                currentZombiePosition = 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, walkPoints[currentZombiePosition].transform.position, Time.deltaTime * zombieSpeed);
+        //change zombie facing
+    }
+    private void Pursueplayer()
+    {
+        zombieAgent.SetDestination(playerBody.position);
+    }
+}
